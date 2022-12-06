4f713dbbb89c839d024fd5755ff790eb34e52872
Yohan <pyh95510@gmail.com>
2022-12-05 20:20:24 -0500

    password encrypted

--- src/main/java/com/cos/photostagram/config/SecurityConfig.java
+++ src/main/java/com/cos/photostagram/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.cos.photostagram.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // activate security file.
 @Configuration // register to IoC container, now it will be managed by the IoC container.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // if we use Bean annotation, when security config is registered into IoC
+    // container, it will recognize Bean annotation, and will return
+    // BcryptPasswordEncoder
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(); // literally encrypt password
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/cos/photostagram/service/AuthService.java
+++ src/main/java/com/cos/photostagram/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.cos.photostagram.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photostagram.domain.user.User;
 import com.cos.photostagram.domain.user.UserRepository;
@@ -11,9 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class AuthService {
-    final UserRepository userRepository; // di
+    private final UserRepository userRepository; // DI
+    private final BCryptPasswordEncoder bCryptPasswordEncoder; // DI bc of RequiredArgsConstructor
 
+    @Transactional
     public User Singups(User user) {
+
+        String rawpassword = user.getPassword(); // bring password from dto
+        String encPassword = bCryptPasswordEncoder.encode(rawpassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
+
         User userEntity = userRepository.save(user); // save(s type) here, s type is the type that I set, here is user
 
         return userEntity;
