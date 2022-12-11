0d8a397cddfa390899e86bc1af3dadbd034d9bee
Yohan <pyh95510@gmail.com>
2022-12-11 14:55:57 -0500

    make CMRespDto using generic

--- src/main/java/com/cos/photostagram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photostagram/handler/ControllerExceptionHandler.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photostagram.handler.ex.CustomValidationException;
+import com.cos.photostagram.web.dto.auth.CMRespDto;
 
 @RestController // by using rest controller, when error happens, this page is shown
 @ControllerAdvice // collect all exceptions here
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CMRespDto<Map<String, String>> validationException(CustomValidationException e) {
+        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()); // if it is succedded 1, fail -1. it is just http
+                                                                     // protocol.
     }
 
 }
--- src/main/java/com/cos/photostagram/web/dto/auth/CMRespDto.java
+++ src/main/java/com/cos/photostagram/web/dto/auth/CMRespDto.java
@@ -9,8 +9,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class CMRespDto {
+public class CMRespDto<T> {
+    private Integer code;
     private String message;
-    private Map<String, String> errorMap;
+    private T data;
+
+    // we have created CMRespDto class to return both message and erroMap. Now, we
+    // have to change the return type for erro type to CMRespDto
 
 }
+/*
+ * Why are we using generic T here? we cannot always response to error data
+ * type. Sometimes we need to response to member data and various
+ * data. In that case we cannot always change the CMRespDto. To consider all of
+ * those cases, the best way to address is using generics
+ * 
+ */
