6070445dfe75776eaec38df51e7a80121426cf2d
Yohan <pyh95510@gmail.com>
2022-12-15 21:34:03 -0500

    change CMRespDto to javascript type

--- src/main/java/com/cos/photostagram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photostagram/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photostagram.handler.ex.CustomValidationException;
+import com.cos.photostagram.util.Script;
 import com.cos.photostagram.web.dto.auth.CMRespDto;
 
 @RestController // by using rest controller, when error happens, this page is shown
@@ -14,9 +15,12 @@ import com.cos.photostagram.web.dto.auth.CMRespDto;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e) {
-        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()); // if it is succedded 1, fail -1. it is just http
-                                                                     // protocol.
+    public String validationException(CustomValidationException e) {
+        return Script.back(e.getMessage().toString());
+
+        // if it is succedded 1, fail -1. it is just http. For ajax or developers,
+        // CMRespDto is better, however, for the users, Script is better
+        // protocol.
     }
 
 }
