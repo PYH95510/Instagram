58d75c5d4c14e76aaa9e87d6f0b1ee095d423682
Yohan <pyh95510@gmail.com>
2022-12-24 18:08:44 -0500

    using path variable, and id variable to create more than one profiles

--- src/main/java/com/cos/photostagram/web/UserController.java
+++ src/main/java/com/cos/photostagram/web/UserController.java
@@ -2,12 +2,13 @@ package com.cos.photostagram.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/user/profile")
-    public String profile() {
+    @GetMapping("/user/{id}") // it should be existed as variable because there should be more than one users.
+    public String profile(@PathVariable Integer id) {
         return "user/profile";
     }
 }
