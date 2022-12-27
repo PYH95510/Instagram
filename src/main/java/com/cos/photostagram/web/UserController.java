1b64d007b912986fe9980a0c3457916b9e1af98f
Yohan <pyh95510@gmail.com>
2022-12-26 20:47:06 -0500

    checking session information

--- src/main/java/com/cos/photostagram/web/UserController.java
+++ src/main/java/com/cos/photostagram/web/UserController.java
@@ -1,8 +1,11 @@
 package com.cos.photostagram.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photostagram.config.auth.PrincipalDetails;
 
 @Controller
 public class UserController {
@@ -13,7 +16,8 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable Integer id) {
+    public String update(@PathVariable Integer id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println("Verifying session information" + principalDetails.getUser());
         return "user/update";
     }
 
